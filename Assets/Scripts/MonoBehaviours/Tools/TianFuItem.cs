61d4a39b2ac2e730ac5bd6e7e440d150e291d3d3
lli <793437707@qq.com>
2023-05-05 02:53:29 +0800

    天赋 UI修改，基础script

---
+++ Assets/Scripts/MonoBehaviours/Tools/TianFuItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TianFuItem : MonoBehaviour
+{
+    int id;
+    private void Awake()
+    {
+        
+    }
+
+    public void Init(int id = -1)
+    {
+        if (id == -1)
+            id = this.id;
+        this.id = id;
+    }
+}
---
+++ Assets/Scripts/MonoBehaviours/Tools/TianFuUI.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+public class TianFuUI : MonoBehaviour
+{
+    public GameObject prefab;
+    bool isInit = false;
+    Transform Content;
+    List<TianFuItem> items = new List<TianFuItem>();
+    Text CoinText;
+    Vector3 pos;
+
+    private void Awake()
+    {
+        Content = transform.Find("Scroll View/Viewport/Content");
+        pos = Content.GetComponent<RectTransform>().position;
+        CoinText = transform.Find("CoinItem/cost").GetComponent<Text>();
+    }
+
+    private void LateUpdate()
+    {
+        CoinText.text = GameData.Inst.GlodCoin.ToString();
+    }
+
+    public void Show()
+    {
+        if (!isInit)
+        {
+            InitItems();
+            isInit = true;
+        }
+        for (int i = 0; i < items.Count; i++)
+        {
+            items[i].Init();
+        }
+        Content.GetComponent<RectTransform>().position = pos;
+    }
+
+    void InitItems()
+    {
+        for (int i = 0; i < GameManager.databasesManager.TianFuGetSize(); i++)
+        {
+            var item = Instantiate(prefab, Content);
+            items.Add(item.GetComponent<TianFuItem>());
+            item.GetComponent<TianFuItem>().Init(i);
+        }
+    }
+}
