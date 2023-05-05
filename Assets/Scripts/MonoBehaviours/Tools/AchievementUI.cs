55429bc703b4cd687808b843111e18994058c016
lli <793437707@qq.com>
2023-05-05 23:39:38 +0800

    成就系统也添加剩余金币显示

--- Assets/Scripts/MonoBehaviours/Tools/AchievementUI.cs
+++ Assets/Scripts/MonoBehaviours/Tools/AchievementUI.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 
 public class AchievementUI : MonoBehaviour
@@ -10,12 +11,20 @@ public class AchievementUI : MonoBehaviour
     bool isInit = false;
     Transform Content;
     List<AchievementItem> items = new List<AchievementItem>();
+    Text CoinText;
     Vector3 pos;
+    int coin;
 
     private void Awake()
     {
         Content = transform.Find("Scroll View/Viewport/Content");
         pos = Content.GetComponent<RectTransform>().position;
+        CoinText = transform.Find("CoinItem/cost").GetComponent<Text>();
+    }
+
+    private void LateUpdate()
+    {
+        UpdateCoin();
     }
 
     public void Show()
@@ -36,6 +45,15 @@ public class AchievementUI : MonoBehaviour
         {
             items[i].transform.SetSiblingIndex(i);
         }
+    }
+
+    void UpdateCoin()
+    {
+        if (coin == GameData.Inst.GlodCoin)
+            return;
+        //金币变动，更新显示属性以及能否购买
+        coin = GameData.Inst.GlodCoin;
+        CoinText.text = coin.ToString();
     }
 
     void InitItems()
